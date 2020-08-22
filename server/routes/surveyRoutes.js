2443998ceea84afad0d005dabfd2d6c59a394f47
sherg21 <ssgill@princeton.edu>
2020-08-22 14:10:48 -0700

    testing surveys endpoint 404 error

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,8 +10,11 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = (app) => {
-  app.get("/api/surveys/:surveyId/:choice", (req, res) => {
-    res.send("Thanks for voting!");
+  app.get("/api/surveys/:surveyId/:choice", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false,
+    });
+    res.send(surveys);
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
